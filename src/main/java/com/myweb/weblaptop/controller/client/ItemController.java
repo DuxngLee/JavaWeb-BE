ff68447ff932f932719c2f04feaa56a6705a9798
Lê Dũng <113307765+Leeduxng@users.noreply.github.com>
2025-06-08 01:02:44 +0700

    success product detail

--- src/main/java/com/myweb/weblaptop/controller/client/ItemController.java
+++ src/main/java/com/myweb/weblaptop/controller/client/ItemController.java
@@ -1,14 +1,26 @@
 package com.myweb.weblaptop.controller.client;
 
-import ch.qos.logback.core.model.Model;
+
+import com.myweb.weblaptop.domain.Product;
+import com.myweb.weblaptop.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ItemController {
+
+    private final ProductService productService;
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getItem(@PathVariable Long id, Model model) {
+        Product pr = this.productService.fetchProductById(id).get();
+        model.addAttribute("product", pr);
+        model.addAttribute("id", id);
         return "client/product/detail";
     }
 
